336367d3fbc848bcbb620aabc07566e69e427dd3
Timox134 <77526270+Timox134@users.noreply.github.com>
2023-07-13 17:31:17 +0300

    d

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 //! A bot that logs chat messages sent in the server to the console.
 
+use std::ptr::hash;
 use azalea::prelude::*;
 use parking_lot::Mutex;
 use std::sync::Arc;
+use azalea::ecs::prelude::With;
+use azalea::entity::Position;
+use azalea::entity::metadata::Player;
+use azalea::{BlockPos, GameProfileComponent};
+use azalea::pathfinder::BlockPosGoal;
 
 #[tokio::main]
 async fn main() {
-    let account = Account::offline("bot");
+    let account = Account::offline("botbot");
     // or Account::microsoft("example@example.com").await.unwrap();
 
     loop {
@@ -21,10 +27,33 @@ async fn main() {
 #[derive(Default, Clone, Component)]
 pub struct State {}
 
-async fn handle(bot: Client, event: Event, state: State) -> anyhow::Result<()> {
+async fn handle(mut bot: Client, event: Event, state: State) -> anyhow::Result<()> {
     match event {
         Event::Chat(m) => {
-            println!("{}", m.message().to_ansi());
+            let Some(sender) = m.username()  else { return Ok(()) };
+                    let entity = bot.entity_by::<With<Player>, (&GameProfileComponent, )>(
+                        |profile: &&GameProfileComponent| {
+                            println!("entity {profile:?}");
+                            profile.name == sender
+                        },
+                    );
+                    if let Some(entity) = entity {
+                        match m.content().as_str() {
+                            "here" => {
+                                let pos = bot.entity_component::<Position>(entity);
+                                let target_pos: BlockPos = pos.into();
+                                bot.goto(BlockPosGoal::from(target_pos));
+                            }
+                            _ => {}
+                        }
+                    }
+
+        }
+        Event::Login => {
+            let a = &bot.profile.name;
+            bot.chat(&*format!("/register {}ab", a));
+            bot.chat(&*format!("/login {}ab", a));
+            bot.chat("Hello world");
         }
         _ => {}
     }
